843ee5bff2e663e52e4f184ee8d53e6aca1ef789
Ondrej Bruha <ondrej.bruha@alphacodes.eu>
2024-02-22 00:57:33 +0100

    progress

---
+++ du1/du1.py
@@ -0,0 +1,38 @@
+import random
+import numpy as np
+import cv2 as cv
+
+
+def generate_random_population(num):
+    population = []
+    for i in range(num):
+        random_number = random.randint(1, 2)
+        if i % random_number == 0:
+            population.append(1)
+        else:
+            population.append(0)
+    return population
+
+
+def step(prev):
+    plus = []
+    for i in range(len(prev)):
+        if i < len(prev) - 1:
+            plus.append(prev[i] * prev[i + 1])
+        else:
+            plus.append(prev[i] * prev[0])
+    return plus
+
+
+def main():
+    population = [generate_random_population(100)]
+    steps = 100
+    for i in range(steps):
+        population.append(step(population[i]))
+    arr = np.array(population)
+    cv.normalize(arr, arr, 0, 255, cv.NORM_MINMAX)
+    cv.imwrite("output.png", arr)
+
+
+if __name__ == '__main__':
+    main()
